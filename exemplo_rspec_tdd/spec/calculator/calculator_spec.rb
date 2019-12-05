597e66556fa4629a32220066653e715debb2e6c6
Agnaldo Burgo Junior <agnaldo.junior@yapay.com.br>
2019-12-05 16:27:22 -0300

    add errors matchers

--- exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
+++ exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
@@ -18,7 +18,15 @@ describe Calculator, 'about the calculator' do
       result = subject.sum(-5, -7)
       expect(result).to eq(-12)
     end
+  end
 
-
+  context '#div' do
+      it 'divide by 0' do
+        #expect{subject.div(3, 0)}.to raise_exception
+        expect{subject.div(3, 0)}.to raise_error(ZeroDivisionError)
+        expect{subject.div(3, 0)}.to raise_error('divided by 0')
+        expect{subject.div(3, 0)}.to raise_error(ZeroDivisionError, 'divided by 0')
+        expect{subject.div(3, 0)}.to raise_error(/divided/)
+      end
   end
 end
