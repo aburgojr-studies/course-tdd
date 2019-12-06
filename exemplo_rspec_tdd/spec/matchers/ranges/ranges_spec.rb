81f99fdfe36781166fbcc373b47bab370a679f16
Agnaldo Burgo Junior <agnaldo.junior@yapay.com.br>
2019-12-06 16:54:36 -0300

    understanding only line sintaxe

--- exemplo_rspec_tdd/spec/matchers/ranges/ranges_spec.rb
+++ exemplo_rspec_tdd/spec/matchers/ranges/ranges_spec.rb
@@ -5,3 +5,18 @@ describe (1...5), 'ranges' do
     expect(subject).not_to cover(0, 6)
   end
 end
+
+describe (1..5), 'ranges 2' do
+  it '#cover' do
+    is_expected.to cover(2) 
+    is_expected.to cover(2,4)
+    is_expected.not_to cover(0, 6)
+  end
+
+  #only line sintaxe
+  it { is_expected.to cover(2) }
+  it { is_expected.to cover(2,5) }
+  it { is_expected.not_to cover(0,6) }
+  it { expect(subject).to cover(2) }
+end
+
